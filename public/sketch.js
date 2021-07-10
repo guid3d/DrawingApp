c7fac71071d812498e2b673628b7f3ebff79b457
Guid3d <princegag1@gmail.com>
2021-07-10 23:18:20 +0200

    Make full screen

--- public/sketch.js
+++ public/sketch.js
@@ -5,7 +5,7 @@ let cv
 
 function setup() {
 	// Creating canvas
-	cv = createCanvas(windowWidth / 2, windowHeight / 2)
+	cv = createCanvas(windowWidth, windowHeight)
 	centerCanvas()
 	cv.background(255, 255, 255)
 
@@ -20,29 +20,29 @@ function setup() {
 	})
 
 	// Getting our buttons and the holder through the p5.js dom
-	const color_picker = select('#pickcolor')
-	const color_btn = select('#color-btn')
-	const color_holder = select('#color-holder')
+	// const color_picker = select('#color-picker')
+	// const color_btn = select('#color-btn')
+	// const color_holder = select('#color-holder')
 
-	const stroke_width_picker = select('#stroke-width-picker')
-	const stroke_btn = select('#stroke-btn')
+	// const stroke_width_picker = select('#stroke-width-picker')
+	// const stroke_btn = select('#stroke-btn')
 
-	// Adding a mousePressed listener to the button
-	color_btn.mousePressed(() => {
-		// Checking if the input is a valid hex color
-		if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
-			color = color_picker.value()
-			color_holder.style('background-color', color)
-		} else {
-			console.log('Enter a valid hex value')
-		}
-	})
+	// // Adding a mousePressed listener to the button
+	// color_btn.mousePressed(() => {
+	// 	// Checking if the input is a valid hex color
+	// 	if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
+	// 		color = color_picker.value()
+	// 		color_holder.style('background-color', color)
+	// 	} else {
+	// 		console.log('Enter a valid hex value')
+	// 	}
+	// })
 
-	// Adding a mousePressed listener to the button
-	stroke_btn.mousePressed(() => {
-		const width = parseInt(stroke_width_picker.value())
-		if (width > 0) strokeWidth = width
-	})
+	// // Adding a mousePressed listener to the button
+	// stroke_btn.mousePressed(() => {
+	// 	const width = parseInt(stroke_width_picker.value())
+	// 	if (width > 0) strokeWidth = width
+	// })
 }
 
 function windowResized() {
